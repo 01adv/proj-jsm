91c168d78dda61a2260b51be2bfb4db5325457d4
Advaita <148793147+01adv@users.noreply.github.com>
2024-06-06 00:53:06 +0530

    done upto clerk auth

---
+++ ai_podcast/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from '@clerk/nextjs'
+
+const page = () => {
+  return (
+    <div className='flex-center glassmorphism-auth h-screen w-full'><SignUp/></div>
+  )
+}
+
+export default page
---
+++ ai_podcast/app/(root)/layout.tsx
@@ -0,0 +1,30 @@
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSideBar from "@/components/RightSideBar";
+import Image from "next/image";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="relative flex flex-col">
+      <main className="relative flex bg-black-3">
+        <LeftSidebar />
+        <section className="border-2 border-orange-400 flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+          <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+            <div className="flex h-16 items-center justify-between md:hidden">  
+              <Image src={'/icons/logo.svg'}width={30} height={30} alt="menu icon"/>
+              mobile nav
+            </div>
+            <div className="flex flex-col md:pb-14">
+              Toaster (notification) from shadcn
+              {children}
+            </div>
+          </div>
+        </section>
+        <RightSideBar/>
+      </main>
+    </div>
+  );
+}
